ed0a3ac73d87aa141c377ba4c24880caf112d932
Robert Thatcher <thatcr@googlemail.com>
2016-11-07 18:28:22 +0000

    redux: context is just a dict indexed by obj, desc node holder just implements getattr and setattr

--- src/tests/tradable_test.py
+++ src/tests/tradable_test.py
@@ -7,56 +7,29 @@ logging.basicConfig(level=logging.DEBUG)
 
 node_context = threading.local()
 
-class LoggingContext(object):
-    indent = 0
-    def __init__(self, obj, desc):
-        self.obj = obj
-        self.desc = desc
+class LoggingContext(dict):
+    def __init__(self):
+        self.indent = 0
 
-    def __enter__(self):
-        logging.debug('    ' * self.indent + repr((self.obj, self.desc)))
-        self.__class__.indent += 1
+    def __getitem__(self, key):
+        obj, desc = key
+        logging.debug('    ' * self.indent + repr((obj, desc)))
+        try:
+            self.indent += 1
+            return desc.__get__(obj)
+        finally:
+            self.indent -= 1
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.__class__.indent -= 1
+    def __setitem__(self, key, value):
+        assert self.indent == 0, 'attempted to set node as part of an evaluation'
+        logging.debug('{!r} = {!r}'.format(key, value))
 
-node_context.context = LoggingContext
+        obj, desc = key
+        desc.__set__(obj, value)
+
+node_context.context = LoggingContext()
 
 # how do we represnet nodes with argumnets: assignment syntax is awkwards
-
-'''
-foo.Currency['x', 1, 2]  =
-foo.Currency['x', 3, 4]
-
-__getitem__, __setitem__, __delitem__
-
-use slice syntax? allow for mnulticalls with slicing, nicely?
-
-'''
-
-'''
-better way
-
--  need objects to be singleone on __new__, with avoided __init_, soo x('A") is x('A'),
-- the we have a better equivalence for caching:
-
-
-- remap the Thing call into an object with a property of that name.
-def Thing(self, arg1, arg2):
-    ....
-
-- ThingCaller(self, arg1, arg2).Thing
-
-THat means that the cache still only deals with object, descriptor as keps/values of nodes.
-The name resolution logic sits in the getter. But we have to remmeber the sets of argumenst that were invoked.
-
-- Or a better syntax?
-
-self[arg1, arg2].Thing
-
-- define a cached space of called object with arguments,
-
-'''
 
 class NodeMetaClass(type):
     def __new__(cls, name, bases, nmspc):
@@ -67,23 +40,32 @@ class NodeMetaClass(type):
             # what it the value is a node descriptor from _another_ class? do we make our own...
             # getters on that will get an unexpected instance, which may be ok
 
-            nmspc[key] = type(key + 'Descriptor', (property,), {})(
+            nmspc[key] = type(key, (property,), {})(
                 fget=value.fget, fset=value.fset, fdel=value.fdel, doc=value.__doc__
             )
 
         return super().__new__(cls, name, bases, nmspc)
 
 class NodeBase(object, metaclass=NodeMetaClass):
-    trace = [[]]
+    '''
+    redirect ay access to properties on this class through the node context
+    which is a dict indexed by object, descriptor that can capture the node
+    evaluation graph and provide cached values
+    '''
 
     def __getattribute__(self, item):
         desc = getattr(super().__getattribute__('__class__'), item, None)
         if isinstance(desc, property):
+            return node_context.context[self, desc]
+        return super().__getattribute__(item)
+
+    def __setattr__(self, item, value):
+        desc = getattr(super().__getattribute__('__class__'), item, None)
+        if isinstance(desc, property):
             # should we stack up contexts here? or use the same one?
-            with node_context.context(self, desc):
-                return super().__getattribute__(item)
-        else:
-            return super().__getattribute__(item)
+            node_context.context[self, desc] = value
+            return
+        return super().__setattr__(self, item, value)
 
 
 node = property
