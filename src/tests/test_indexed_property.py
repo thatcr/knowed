beb6a6dc2fa8ffb6157694ae844d96a8af57bb00
Robert Thatcher <thatcr@googlemail.com>
2016-11-11 07:25:06 +0000

    benchmarking call overheads, roughly 8x bare implementation (compare with a method?)

--- src/tests/test_indexed_property.py
+++ src/tests/test_indexed_property.py
@@ -6,14 +6,14 @@ class IndexedPropertyMapper(object):
         self.instance = instance
 
     def __getitem__(self, item):
-        return self.desc.fget(self, item)
+        return self.desc.fget(self.instance, item)
 
     def __setitem__(self, item, value):
         # hmm. is this order of arguments right?
-        self.desc.fset(self, value, item)
+        self.desc.fset(self.instance, value, item)
 
     def __delitem__(self, item):
-        self.desc.fdel(self, item)
+        self.desc.fdel(self.instance, item)
 
 class MultiIndexedPropertyMapper(object):
     def __init__(self, desc, instance):
@@ -21,14 +21,14 @@ class MultiIndexedPropertyMapper(object):
         self.instance = instance
 
     def __getitem__(self, item):
-        return self.desc.fget(self, *item)
+        return self.desc.fget(self.instance, *item)
 
     def __setitem__(self, item, value):
         # hmm. is this order of arguments right?
-        self.desc.fset(self, *item, value)
+        self.desc.fset(self.instance, *item, value)
 
     def __delitem__(self, item):
-        self.desc.fdel(self, *item)
+        self.desc.fdel(self.instance, *item)
 
 # could we allow __delete__ to invalidate all nodes?
 # what does __set__ do? assign the whole mapping? that sounds ok.
@@ -43,7 +43,7 @@ class MultiIndexedPropertyDescriptor(property):
     def __get__(self, instance, owner):
         return MultiIndexedPropertyMapper(self, instance)
 
-def indexed_property(fget, *args, **kwargs):
+def index(fget, *args, **kwargs):
 
     if fget.__code__.co_argcount > 2:
         return MultiIndexedPropertyDescriptor(fget, *args, **kwargs)
@@ -53,11 +53,11 @@ def indexed_property(fget, *args, **kwargs):
 
 def test_indexed_property():
     class Thingy(object):
-        @indexed_property
+        @index
         def AddOne(self, i):
             return i + 1
 
-        @indexed_property
+        @index
         def AddTwo(self, i, j):
             return i+j
 
@@ -73,3 +73,38 @@ def test_indexed_property():
     assert t.AddOne[3] == 4
     assert t.AddTwo[2,3] == 5
 
+
+class FibonacciThingy(object):
+    @index
+    def Fib(self, item):
+        if item < 0:
+            raise KeyError('must be bigger than 0')
+        if item == 0 or item == 1:
+            return 1
+        return self.Fib[item - 1] + self.Fib[item - 2]
+
+def test_fibonacci():
+    t = FibonacciThingy()
+
+    with raises(KeyError):
+        t.Fib[-100]
+
+    assert t.Fib[0] == 1
+    assert t.Fib[1] == 1
+    assert t.Fib[6] == 13
+
+
+def test_benchmark_fibonacci(benchmark):
+    t = FibonacciThingy()
+    benchmark(lambda : t.Fib[20])
+    assert False
+
+def barefaced_fib(item):
+    if item < 0:
+        raise KeyError('must be bigger than 0')
+    if item == 0 or item == 1:
+        return 1
+    return barefaced_fib(item - 1) + barefaced_fib(item - 2)
+
+def test_benchmark_barefaced_fib(benchmark):
+    benchmark(lambda : barefaced_fib(20))
