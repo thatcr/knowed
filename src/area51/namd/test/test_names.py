5a8d4c0e62a1fc9a596dae8ba943e2620b376f2c
Robert Thatcher <thatcr@googlemail.com>
2016-11-24 18:11:34 +0000

    increase test coverage

--- src/area51/namd/test/test_names.py
+++ src/area51/namd/test/test_names.py
@@ -1,8 +1,12 @@
 from ..base import NamdObject
 
+
 def test_names():
     class Thing(NamdObject):
         pass
 
-    assert str(Thing()) ==  'Thing'
+    assert str(Thing()) == 'Thing'
     assert str(Thing('ABC')) == 'Thing ABC'
+
+    assert repr(Thing()) == __name__ + '.' + Thing.__name__ + '()'
+    assert repr(Thing('ABC')) == __name__ + '.' + Thing.__name__ + "('ABC',)"
